1c749c9914868c6263cb10b1338298b66bc2cc62
riteshdhemla <riteshdhemla@gmail.com>
2024-09-18 02:40:16 +0530

    add clock time for vector addition

--- vecAddMain.c
+++ vecAddMain.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+#include <time.h>
 
 __global__ void vecAddKernel(float* A_d, float* B_d, float* C_d, int n);
 
@@ -11,9 +12,10 @@ int main()
 {
     float *A_d, *B_d, *C_d;
     int n;
+    clock_t start_time, end_time;
     
-    // scanf("%d", &n);
-    n = 1000;
+    scanf("%d", &n);
+
     int memSize = n * sizeof(float);
     
     A_d = (float*) malloc(memSize); 
@@ -26,7 +28,12 @@ int main()
         B_d[i] = i;
     }
 
+    start_time = clock();
     vecAdd(A_d, B_d, C_d, n);
+    end_time = clock();
+
+    printf("Time Elapsed : %f \n", (double) (end_time - start_time) / CLOCKS_PER_SEC);
+
     for (int i = 0; i < n; ++i)
     {
         if ( i % 100 == 0)
